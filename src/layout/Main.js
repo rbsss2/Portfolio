b38a2d29e82ddcc8a1cc05ffb96f0be1ee0b2c2f
rbsss2 <rbsss2@naver.com>
2023-02-09 10:14:24 +0900

    Hash 태그 수정

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import MainContents from "../components/MainContents";
 
 import "../styles/main.scss";
-import Nav from "./Nav";
+
 function Main(props) {
   return (
     <div className="section sec1 active">
